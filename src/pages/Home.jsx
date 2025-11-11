14300589ce8e4477178033e2a343add1e3e6d337
david-1910 <dovud1.1910@gmail.com>
2025-11-12 04:14:29 +0500

    next commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 
-import Fade from "../components/Animation/Fade";
 import Header from "../components/1_Header/Header";
 import Sidebar from "../components/2_Sidebar/Sidebar";
 import Intro from "../components/3_Intro/Intro";
@@ -18,9 +17,7 @@ export default function Home() {
             <Sidebar open={open} setOpen={setOpen} />
             <main>
                 <Intro />
-                <Fade>
                     <Skills />
-                </Fade>
                 <About />
                 <Contacts />
             </main>
